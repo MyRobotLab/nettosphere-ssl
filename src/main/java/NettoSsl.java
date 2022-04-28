99d0a934ecd60b4c935ad6f350f787f7f2ae4d8a
Greg Perry <greg.perry@torc.ai>
2022-04-28 12:37:10 -0700

    example

---
+++ src/main/java/NettoSsl.java
@@ -0,0 +1,38 @@
+import org.atmosphere.nettosphere.Config;
+import org.atmosphere.nettosphere.Nettosphere;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+public class NettoSsl {
+
+  public static void main(String[] args) {
+    try {
+
+      System.out.println("hi");
+
+      Config.Builder configBuilder = new Config.Builder();
+
+      boolean ssl = true;
+      if (ssl) {
+        SelfSignedCertificate ssc = new SelfSignedCertificate("localhost");
+        // SslContext sslCtx = SslContextBuilder.forServer(new File(certPath),
+        // new
+        // File(keyPath), null).build();
+        SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey(), null).build();
+        configBuilder.sslContext(sslCtx);
+      }
+
+      configBuilder.resource("./");
+      Nettosphere nettosphere = new Nettosphere.Builder().config(configBuilder.build()).build();
+      nettosphere.start();
+
+    } catch (Exception e) {
+      System.out.println("main threw");
+      e.printStackTrace();
+    }
+
+  }
+
+}
